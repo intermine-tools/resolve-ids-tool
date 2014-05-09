83023b810854676bcd10373db44fafd6b28c62ac
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-05-09 18:58:22 +0100

    Working version

---
+++ index.js
@@ -0,0 +1,96 @@
+var chan = Channel.build({
+  window: document.getElementById("child").contentWindow,
+  origin: "*",
+  scope: "CurrentStep"
+});
+
+var root = "http://www.flymine.org/query/service";
+
+chan.call({
+  method: 'configure',
+  params: {
+    matchViewStrategy: 'full'
+  },
+  success: function () {
+    console.log("Configured");
+  }
+});
+
+chan.bind('has-ids', function (trans, data) {
+  console.log("HAS IDS!!");
+  handleIds(data.objectIds);
+});
+
+chan.bind('has-item', function (trans, data) {
+  console.log("HAS ITEM!!");
+  portal(data.object);
+});
+
+getJSON(root + "/session", function withSession (response) {
+
+  chan.call({
+    method: "init",
+    params: {
+      service: {
+        root: root,
+        token: response.token
+      },
+      request: {
+        type: 'Gene',
+        extra: 'D. melanogaster',
+        caseSensitive: false,
+        identifiers: [
+          'CG9151',
+          'FBgn0000099',
+          'CG3629',
+          'TfIIB',
+          'Mad',
+          'CG1775',
+          'CG2262',
+          'TWIST_DROME',
+          'tinman',
+          'runt',
+          'E2f',
+          'CG8817',
+          'FBgn0010433',
+          'CG9786',
+          'CG1034',
+          'ftz',
+          'FBgn0024250',
+          'FBgn0001251',
+          'tll',
+          'CG1374',
+          'CG33473',
+          'ato',
+          'so',
+          'CG16738',
+          'tramtrack',
+          'CG2328',
+          'gt'
+        ]
+      }
+    },
+    success: function() {
+      console.log("Tool initialised");
+    }
+  });
+});
+
+function getJSON (url, cb) {
+  var sessionRequest = new XMLHttpRequest();
+  var handler = function (e) { cb(sessionRequest.response); };
+
+  sessionRequest.onload = handler;
+  sessionRequest.open('GET', url, true);
+  sessionRequest.responseType = 'json';
+
+  sessionRequest.send();
+}
+
+function handleIds (objectIds) {
+  console.log("These ones:", objectIds);
+}
+
+function portal (object, el) {
+  console.log("Thinking with portals", object);
+}
---
+++ tool.js
@@ -0,0 +1,105 @@
+'use strict';
+
+/**
+ * Tool for resolving ids.
+ *
+ * Listens for:
+ *   - configure
+ *   - init
+ * Emits:
+ *   - has-ids
+ *   - has-item
+ */
+
+var chan, options, spinner, spinnerEl;
+
+options = {
+  // Show/hide a button to download the summary?
+  'showDownloadSummary': true,
+  // How do we show the matches?
+  // [full] - show all summary fields in the table
+  // [slim] - show only a symbol and provide more info in a popover
+  'matchViewStrategy': 'full'
+};
+
+spinner = new Spinner({
+  color: '#000',
+  top: '250px',
+  radius: 100,
+  length: 100,
+  width: 50,
+  lines: 12
+});
+
+spinnerEl = document.getElementById('spinner');
+spinner.spin();
+spinnerEl.appendChild(spinner.el);
+
+chan = Channel.build({
+  window: window.parent,
+  origin: "*",
+  scope: "CurrentStep"
+});
+
+
+chan.bind('configure', function (trans, params) {
+  if (params.showDownloadSummary) {
+    options.showDownloadSummary = params.showDownloadSummary;
+  }
+  if (params.matchViewStrategy) {
+    options.matchViewStrategy = params.matchViewStrategy;
+  }
+  return 'ok';
+});
+
+chan.bind('init', function (trans, params) {
+  var request = params.request;
+  var service = imjs.Service.connect(params.service);
+
+  var jobbing = service.resolveIds(request);
+
+  jobbing.then(function (job) {
+    job.poll().then(withResults);
+  });
+
+  function withResults (results) {
+    console.log("Booyah: ", results);
+    var mr400 = require('component-400');
+
+    mr400({
+      data: results,
+      el: document.getElementById('id-resolver'),
+      cb: handleIds,
+      portal: portal,
+      options: options
+    });
+    
+  }
+
+  function handleIds (objectIds) {
+    chan.notify({
+      method: 'has-ids',
+      params: {
+        objectIds: objectIds,
+        type: request.type,
+        service: {
+          root: service.root
+        }
+      }
+    });
+  }
+
+  function portal (object, el) {
+    chan.notify({
+      method: 'has-item',
+      params: {
+        object: object,
+        service: {
+          root: service.root
+        }
+      }
+    });
+  }
+
+});
+
